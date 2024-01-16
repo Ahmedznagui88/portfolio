62b9eb69d8e6c8f36fdb5cbfb9c9abd14980e227
Ahmed Znagui <amed.bob88@gmail.com>
2024-01-16 22:08:52 +0100

    start expertise page

--- resources/views/expertise.blade.php
+++ resources/views/expertise.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
 
+    <div id="cardCont" class="container-3 ">        
+        <div  class="row d-flex justify-content-start">           
+          <div class="div-cont col-sm-5 border-none d-flex justify-content-start align-items-start ">
+            <h2 class="expertise">expertise</h2>
+          </div>
+        </div>
+      </div> 
+
+      <div class="scroll-watcher">
+
+      </div>
+
+      <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+      </div>
+
 </x-layout>
