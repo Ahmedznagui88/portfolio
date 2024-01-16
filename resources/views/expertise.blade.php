bd4bd35d0007429a93374a25fca14d92fd4cd62b
Ahmed Znagui <amed.bob88@gmail.com>
2024-01-16 23:00:45 +0100

    prima parte ex

--- resources/views/expertise.blade.php
+++ resources/views/expertise.blade.php
@@ -12,10 +12,36 @@
 
       </div>
 
-      <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                
+      <div class="container-fluid cont-expertise ">
+        <div class="row row-expertise">
+            <div class="col-md-6 div-expertise">
+                <h6>front-end</h6>
+            </div>
+        </div>
+
+        <div class="row row-expertise-2">
+            <div class="col-md-6 div-expertise-2 text-center">
+                <h5>html</h5>
+                <h5>css</h5>
+                <h5>bootstrap</h5>
+                <h5>javascript</h5>
+                <h5>react</h5>
+            </div>
+        </div>
+      </div>
+
+      <div class="container-fluid cont-expertise ">
+        <div class="row row-expertise">
+            <div class="col-md-6 div-expertise">
+                <h6>back-end</h6>
+            </div>
+        </div>
+
+        <div class="row row-expertise-2">
+            <div class="col-md-6 div-expertise-2 text-center d-flex align-items-center">
+                <h5>php</h5>
+                <h5>laravel</h5>
+                <h5>mySql</h5>
             </div>
         </div>
       </div>
