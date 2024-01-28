4d7312065a353d27c0391d7cce38e65b939c3dde
Ahmed Znagui <amed.bob88@gmail.com>
2024-01-29 00:17:28 +0100

    change bg

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -5,10 +5,10 @@ import { TextPlugin } from "gsap/TextPlugin";
 
 gsap.registerPlugin(EaselPlugin,TextPlugin);
 
+
 const tl = gsap.timeline({
     
 });
 
 tl.from(".expertise, h6", {opacity:0, y:50, duration:2,  ease: "circ.out",});
 tl.to(".expertise, h6", {opacity:1, ease: "circ.out",});
-
