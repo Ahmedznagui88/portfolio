35a50c7f979428b3ad0b14a2871d81046f4f36c3
Ahmed Znagui <amed.bob88@gmail.com>
2024-02-07 19:31:37 +0100

    add play and pause svg

--- resources/js/music.js
+++ resources/js/music.js
@@ -28,33 +28,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function startAudio(audio, button, progress) {
         audio.play();
-        button.textContent = 'Pause';
+        button.innerHTML = '<svg width="8" height="13" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="5" width="3" height="16" rx="1" fill="#e4cdc0"/><rect width="3" height="16" rx="1" fill="#e4cdc0"/></svg>';
         button.style.backgroundColor = 'rgba(204,101,000,1)';
         currentPlaying = audio;
-
+    
         audio.addEventListener('timeupdate', updateProgress.bind(null, audio, progress));
         audio.addEventListener('ended', stopAudio.bind(null, audio, button, progress));
     }
 
     function stopAudio(audio, button, progress) {
-        audio.pause();
-        button.textContent = 'Play';
-        button.style.backgroundColor = '';
-        if (progress) progress.value = 0;
-        currentPlaying = null;
+    audio.pause();
+    button.innerHTML = '<svg width="14" height="15" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 7.26795C15.3333 8.03775 15.3333 9.96225 14 10.7321L3.5 16.7942C2.16667 17.564 0.499999 16.6018 0.499999 15.0622L0.5 2.93782C0.5 1.39822 2.16667 0.43597 3.5 1.20577L14 7.26795Z" fill="#e4cdc0"/></svg>';
+    button.style.backgroundColor = '';
+    if (progress) progress.value = 0;
+    currentPlaying = false;
 
-        const nextButton = button.nextElementSibling;
-        if (nextButton) {
-            const nextAudioId = `audio_${nextButton.dataset.audioId}`;
-            const nextProgressId = nextButton.dataset.progressId;
-            playPause(nextAudioId, nextProgressId, nextButton);
-        }
+    const nextButton = button.nextElementSibling;
+    if (nextButton) {
+        const nextAudioId = `audio_${nextButton.dataset.audioId}`;
+        const nextProgressId = nextButton.dataset.progressId;
+        playPause(nextAudioId, nextProgressId, nextButton);
     }
+}
+
+//progress bar
 
     function updateProgress(audio, progress) {
         if (progress) {
             const value = (audio.currentTime / audio.duration) * 100;
-            progress.value = value;
+            progress.querySelector('.progress-bar-fill').style.width = value + '%';
         }
     }
+
 });
+
--- resources/views/expertise.blade.php
+++ resources/views/expertise.blade.php
@@ -104,7 +104,7 @@
 
 
 
-
+{{-- 
 
     <svg class="" width="1425" height="150" viewBox="0 0 1728 151" fill="none" xmlns="http://www.w3.org/2000/svg">
         <ellipse cx="864" cy="400.5" rx="1260" ry="320.5" fill="url(#paint0_linear_999_30)"/>ewrewr
@@ -113,7 +113,7 @@
         <stop stop-color="rgba(0,0,0,1)"/>
         </linearGradient>
         </defs>
-        </svg> 
+        </svg>  --}}
 
     {{--  <x-music /> --}}
 
@@ -141,11 +141,14 @@
                                     <article class="col-3 div-img-tit">
                                         <div class="slides">
                                             <img class="slide" src="{{ $music->cover }}">
+    
                                         </div>
                                         <div class="">
                                             <button class="play-pause-button" data-audio-id="{{ $music->id }}"
                                                 data-progress-id="progress_{{ $music->id }}">
-                                                Play
+                                                <svg width="14" height="15" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M14 7.26795C15.3333 8.03775 15.3333 9.96225 14 10.7321L3.5 16.7942C2.16667 17.564 0.499999 16.6018 0.499999 15.0622L0.5 2.93782C0.5 1.39822 2.16667 0.43597 3.5 1.20577L14 7.26795Z" fill="#e4cdc0"/>
+                                                    </svg>
                                             </button>
                                             <audio id="audio_{{ $music->id }}" class="w-75"
                                                 src="{{ $music->file }}"></audio>
@@ -155,8 +158,10 @@
                                         </div>
                                        
                                         
-                                        <progress class="w-50 mx-5" id="progress_{{ $music->id }}" value="0" max="100"></progress>
-                                    
+{{--                                         <progress class="w-50 mx-5" id="progress_{{ $music->id }}" value="0" max="100"></progress>
+ --}}                                        <div class="custom-progress-bar w-50 mx-5" id="progress_{{ $music->id }}">
+                                            <div class="progress-bar-fill"></div>
+                                        </div>
                                     </article>
                                 @endforeach
                             </div>
