14da33f97ffae27795ef0b9115b67077d099c736
Ahmed Znagui <amed.bob88@gmail.com>
2024-02-12 01:01:42 +0100

    second commit

--- resources/js/exp.js
+++ resources/js/exp.js
@@ -1,74 +1,66 @@
-import { gsap } from "gsap"; 
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 //*prototyping
 let tl = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: ".stack",
-        start:"top 730",
+        start: "top 730",
         end: "top 10",
         scrub: 2,
         /* markers: true, */
     },
 });
 
-tl.to(".stack",{
+tl.to(".stack", {
     ease: "none",
     x: 0,
     y: 0,
-    duration: 10, 
+    duration: 10,
     scale: 4,
-
-  })
-.to(".stack",{
-    
-    duration: 6,
-    markers: true,
-    ease: "none",
 })
-.to(".stack" ,{
-    x: 0,
-    y: 0,
-    duration: 3,
-    opacity: 0,
-});
+    .to(".stack", {
+        duration: 6,
+        markers: true,
+        ease: "none",
+    })
+    .to(".stack", {
+        x: 0,
+        y: 0,
+        duration: 3,
+        opacity: 0,
+    });
 //!end prototyping
-
 
 //*figma
 let tline = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: ".text-figma",
         pin: true,
-  start: "center center",
-  end: "+=560",
+        start: "center center",
+        end: "+=560",
         scrub: 2,
         smooth: 1,
-        effects: true,
-        markers: true,
     },
 });
 
-
-
-tline.to(".text-figma",{
-    ease: "none",
-    x: 0,
-    y: 0,
-    duration: 3, 
-    scale: 2,
-    pin: true,
-    smooth: 1,
-
-  })
-.to(".text-figma" ,{
-    x: 0,
-    y: -200,
-    duration: 3,
-    opacity: 0,
-    color: "#ed9f29",
-});
+tline
+    .to(".text-figma", {
+        ease: "none",
+        x: 0,
+        y: 0,
+        duration: 3,
+        scale: 1.7,
+        pin: true,
+        smooth: 1,
+    })
+    .to(".text-figma", {
+        x: 0,
+        y: 0,
+        duration: 3,
+        opacity: 0,
+        color: "#ed9f29",
+        
+    });
 //!end figma
