6162d7596ab63b46d858968c0b75d65dcac32d22
Ahmed Znagui <amed.bob88@gmail.com>
2024-02-27 16:36:43 +0100

    update

--- resources/js/music.js
+++ resources/js/music.js
@@ -10,6 +10,8 @@ playPauseButtons.forEach(button => {
     });
 });
 
+// play audio
+
 function playPause(audioId, progressId, button) {
     const audio = document.getElementById(audioId);
     const progress = document.getElementById(progressId);
@@ -27,6 +29,7 @@ function playPause(audioId, progressId, button) {
     }
 }
 
+// start audio
 function startAudio(audio, button, progress) {
     audio.play();
     button.innerHTML = '<svg width="8" height="13" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="5" width="3" height="16" rx="1" fill="#e4cdc0"/><rect width="3" height="16" rx="1" fill="#e4cdc0"/></svg>';
@@ -37,6 +40,7 @@ function startAudio(audio, button, progress) {
     audio.addEventListener('ended', stopAudio.bind(false, audio, button, progress));
 }
 
+// stop audio
 function stopAudio(audio, button, progress) {
     audio.pause();
     button.innerHTML = '<svg width="14" height="15" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 7.26795C15.3333 8.03775 15.3333 9.96225 14 10.7321L3.5 16.7942C2.16667 17.564 0.499999 16.6018 0.499999 15.0622L0.5 2.93782C0.5 1.39822 2.16667 0.43597 3.5 1.20577L14 7.26795Z" fill="#e4cdc0"/></svg>';
