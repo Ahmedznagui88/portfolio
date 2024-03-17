df1a15375665ba40eb5a79168665a1cc34ef0a17
Ahmed Znagui <amed.bob88@gmail.com>
2024-03-17 15:56:54 +0100

    add pics

--- resources/js/work.js
+++ resources/js/work.js
@@ -3,27 +3,55 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   //*img project 
 
 
+//? img 0
 
+let imgProject = gsap.timeline ({
+  scrollTrigger: {
+      trigger: ".img-project, .img-project-1",
+      start: "top 320",
+      end: "top 100",
+      scrub: true,
+      ease: "back.out(1.7)"
+  },
+});
 
-  let imgProject = gsap.timeline ({
-    scrollTrigger: {
-        trigger: ".img-project",
-        start: "top 320",
-        end: "top 200",
-        scrub: true,
-        markers: true,
-    },
-  });
+imgProject.to(".img-project, .img-project-1", {
+  x: 0,
+  y: 0,
   
-  imgProject.to(".img-project", {
-    x: 0,
-    y: -20,
-    duration: 2,
-  })
-    .to(".img-project", {
-      y: 0,
-      opacity:0,
+  ease: "back.out(1.7)", // Aggiunta dell'uscita smooth
+})
+.to(".img-project, .img-project-1", {
+  y: 0,
+  opacity: 0,
+  duration: 6,
+  ease: "back.out(1.7)", // Aggiunta dell'uscita smooth
+});
+
+
+
+    //? img 1
+
+ let imgProjectOne = gsap.timeline ({
+      scrollTrigger: {
+          trigger: ".img-project-2",
+          start: "top 520",
+          end: "top 200",
+          scrub: true,
+
+      },
+    });
+    
+    imgProjectOne.to(".img-project-2", {
+      x: 0,
+      y: -250,
+      duration: 6,
+    })
+      .to(".img-project-2", {
         duration: 6,
-        duration: 2,
-        scale: .7,
-    }) 
+        opacity:0,
+         
+          
+          
+      }) 
+
