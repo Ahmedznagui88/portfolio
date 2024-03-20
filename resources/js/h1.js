0cad9228cb98b2fb9507fe55f0c281a1707fb2b0
Ahmed Znagui <amed.bob88@gmail.com>
2024-03-20 23:12:40 +0100

    add animation

--- resources/js/h1.js
+++ resources/js/h1.js
@@ -1,15 +1,24 @@
-
-import { gsap } from "gsap"; 
+import { gsap } from "gsap";
 import { EaselPlugin } from "gsap/EaselPlugin";
 import { TextPlugin } from "gsap/TextPlugin";
 
-gsap.registerPlugin(EaselPlugin,TextPlugin);
+gsap.registerPlugin(EaselPlugin, TextPlugin);
 
+const tml = gsap.timeline({});
 
-const tml = gsap.timeline({
-    
+tml.from(
+    ".name-text, .expertise-2, .about-title, .text-music, .div-svg-identity, .hello",
+    {
+        opacity: -2,
+        y: 50,
+        duration: 1,
+        transformOrigin: "bottom left",
+        stagger: 0.1,
+        ease: "circ.out",
+    }
+);
+tml.to(".name-text, .expertise-2, .div-svg-identity, .hello ", {
+    opacity: 1,
+    stagger: 0.1,
+    ease: "circ.out",
 });
-
-
-tml.from(".name-text, .expertise-2, .about-title, .text-music", {opacity:-2, y:50, duration:1, transformOrigin: "bottom left", stagger: 0.1 , ease: "circ.out",});
-tml.to(".name-text, .expertise-2", {opacity:1, stagger: 0.1 , ease: "circ.out",});
