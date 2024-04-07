c07783661ac62bb1365ea1670205c0bea51d5aea
Ahmed Znagui <amed.bob88@gmail.com>
2024-04-07 14:09:41 +0200

    experience page

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -17,7 +17,6 @@
             </div>
 
            <a href="{{ route('music') }}" class="nav-link col-2   @if (Route::currentRouteName() == 'music') active-1 @endif" target="_blank">
-
                 <p class="bn35">experience</p>
             </a>
         </div>
